9f3dfeda2758754497a019b02171d203227bce75
Dinesh Santhanem <dinesh.santhanem@gmail.com>
2017-07-10 23:50:51 +0530

    Connects to Slack using RTMClient Authentication

--- bin/run.js
+++ bin/run.js
@@ -5,10 +5,16 @@
 'use strict';
 
 const service = require('../server/service');
+const constants = require('../lib/constants');
 const http = require('http');
-const winston = require('winston');
-// winston.log('debug', 'Now my debug messages');
 const server = http.createServer(service);
+const winston = require('winston');
+const slackClient = require('../server/slackClient');
+
+const rtm = slackClient.init(constants.SLACK_BOT_TOKEN, constants.SLACK_LOG_LEVEL);
+rtm.start();
+
+
 
 server.on('listening', function() {
     winston.log('info', `MAYA is listening on port ${server.address().port} in ${service.get('env')} mode...`)
